4afe87b3c671f102c68041c5db4f73c986317517
Kk492000 <kunalkamthe200@gmail.com>
2024-06-16 09:15:49 +0530

    Server code written

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1/${PORT}`);
+});
